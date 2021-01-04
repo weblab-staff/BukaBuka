39094eea7537f026e5ae1d3642ccb14fa2ba85a9
Shannen Wu <shannen@mit.edu>
2021-01-03 19:18:24 -0500

    buka buka is pretty

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -2,10 +2,21 @@
 import React, { useState, useEffect } from 'react';
 
 import axios, { AxiosError } from 'axios';
+import buka_0 from './images/buka_0.png';
+import buka_1 from './images/buka_1.png';
+import buka_2 from './images/buka_2.png';
+import buka_3 from './images/buka_3.png';
+import buka_4 from './images/buka_4.png';
 
-import './app.css';
-import './app.scss';
-import buka from './public/buka.png';
+import './main.scss';
+
+const images = {
+  0: buka_0,
+  1: buka_1,
+  2: buka_2,
+  3: buka_3,
+  4: buka_4,
+};
 
 interface HappinessResponse {
   happiness: number;
@@ -13,7 +24,7 @@ interface HappinessResponse {
 
 export const Main: React.FunctionComponent = () => {
   const [loaded, setLoaded] = useState<boolean>(false);
-  const [happiness, setHappiness] = useState<number>(0);
+  const [happiness, setHappiness] = useState<number>(0.5);
 
   const getHappiness = async () => {
     const happiness = await axios
@@ -23,21 +34,26 @@ export const Main: React.FunctionComponent = () => {
     setHappiness(happiness);
     setLoaded(true);
   };
+
   useEffect(() => {
     getHappiness().catch((err: AxiosError) => {
       throw new Error(`There is no happiness: ${err}`);
     });
   }, []);
 
-  if (!loaded) {
-    return <div>Bukabuka cannot be reached. There is no happiness.</div>;
-  }
+  const buka_number = Math.min(Math.trunc(happiness / 0.2), 4);
+  const buka_size = Math.trunc(happiness * 10) + 1;
+
+  const text = loaded
+    ? "this is buka buka the turtle. buka buka loves questions from web.lab students. no questions make buka buka sad. you don't want to make buka buka sad."
+    : 'buka buka cannot be reached. there is no happiness';
+
   return (
-    <div>
-      <img src={buka} />
-      Bukabuka happiness level: {happiness}
-      <h1>SCSS works</h1>
-      <p>CSS works</p>
+    <div className="dark-blue-bg">
+      <div className="container">
+        <div className="title white-text">{text}</div>
+        <img className={`buka-image-${buka_size}`} src={images[buka_number] || buka_2} />
+      </div>
     </div>
   );
 };
